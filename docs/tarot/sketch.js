705d9ffdb7ea6a580d75c1dd561b817ded2fcc64
permalinku <110309286+permalinku@users.noreply.github.com>
2022-10-08 13:06:13 -0300

    Update sketch.js

--- docs/tarot/sketch.js
+++ docs/tarot/sketch.js
@@ -1,18 +1,71 @@
 
 let snowflakes = []; // array to hold snowflake objects
+let token;
 
 function setup() {
-  createCanvas(400, 600);
+  createCanvas(720, 400);
+  
+  token = new tarotToken();
+  token.setCoords(10, 10);
  
 }
 
 function draw() {
-  background('brown');
+ 
+  background(102);
+  token.update();
+  token.display();
+
+  /*
+  push();
+  translate(width * 0.2, height * 0.5);
   
+  
+  //rotate(frameCount / 200.0);
+  polygon(0, 0, 82, 6);
+  pop();
+  */
+}
+
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function tarotToken() {
+	let coordX = 0;
+	let coordY = 0;
+	let ratio = 82;
+	
+	this.update = function() {
+	
+	};
+	
+	this.display = function() {
+	  push();
+	  translate(coordX, coordY);
+	  polygon(0, 0, 82, 6);
+	  pop();
+	
+	};
+	
+	this.setCoords = function(newX, newY) {
+		this.coordX = newX;
+		this.coordY = newY;
+	
+	};
 }
 
 // snowflake class
 function snowflake() {
+	
   // initialize coordinates
   this.posX = 0;
   this.posY = random(-50, 0);
