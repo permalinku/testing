89b15fffbf27b2097422bb803596580b30336e0e
martingo <110309286+martingo@users.noreply.github.com>
2022-11-16 11:42:29 -0300

    imgs

--- docs/castles/tile.js
+++ docs/castles/tile.js
@@ -2,6 +2,7 @@ function aTile() {
 	 // initialize coordinates
   this.posX = 0;
   this.posY = random(-50, 0);
+  this.myImage;
   
   this.update = function() {
 	  print("tile update");
@@ -9,5 +10,9 @@ function aTile() {
   
   this.draw = function() {
 	  print("tile draw");
+  };
+  
+   this.loadImg = function(daPath) {
+	  print("tile loadImg:" + daPath);
   };
 }
