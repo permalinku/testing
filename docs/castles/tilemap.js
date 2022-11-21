d1e6586957fc63eb1227eb3aac524a78d5432e86
martingo <110309286+martingo@users.noreply.github.com>
2022-11-21 16:27:04 -0300

    Update tilemap.js

--- docs/castles/tilemap.js
+++ docs/castles/tilemap.js
@@ -1,5 +1,7 @@
 function aTilemap() {
-	 // initialize coordinates
+  // initialize coordinates
+  this.rootX = 0;
+  this.rootY = 0;  
   this.posX = 0;
   this.posY = 0;
   this.defaultTilePath;
@@ -7,13 +9,15 @@ function aTilemap() {
   this.wInTiles;
   this.hInTiles;
   
+  this.daTiles = [];
+  
   this.update = function() {
 	  print("tile update");
   };
   
   this.draw = function() {
 	  //print("tile draw");
-	  image(this.myImage, this.posX, this.posY);
+	  //image(this.myImage, this.posX, this.posY);
   };
   
   this.setDefault = function(tilePath) {
@@ -28,10 +32,32 @@ function aTilemap() {
 	  this.defaultTile = loadImage(this.defaultTilePath);
 	  
 	  print("setWorld:" + this.wInTiles + "x" + this.hInTiles);
-	  //this.defaultTile = image(tilePath, 0, 0);
-	  // continuar por aca, luego de las dimensiones hay que continuar con la creacion de tiles
+	  
+	  let w = 0;
+	  
+	  for(let r = 0; r < this.hInTiles; r++) {
+		  
+		  for(let c = 0; c < this.wInTiles; c++) {
+			  w = ((r * this.wInTiles) + c);
+			  
+			  this.daTiles[w] = 0;
+			  //print("r:" + r + " x c:" + c + " i:" + ((r * this.wInTiles) + c) );		
+
+              //print(this.getDaTile(r,c));			  
+		  }		  
+	  }
+	  
+	  print(this.daTiles);
 	  
 	  
+	  
+	  
+  };
+  
+  this.getDaTile = function(rowOfTile, heightOfTile) {
+	  let q = ((rowOfTile * this.wInTiles) + heightOfTile);
+	  return this.daTiles[q];
+	  //print(rowOfTile + " x " + heightInTiles + " = "q + " tile:" + this.daTiles[q]);
   };
   
   
