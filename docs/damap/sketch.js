e053d67c90a767e58e320af602d8982802b9a968
martingo <110309286+martingo@users.noreply.github.com>
2022-12-27 11:43:54 -0300

    Update sketch.js

--- docs/damap/sketch.js
+++ docs/damap/sketch.js
@@ -10,10 +10,14 @@ function draw() {
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
 
-  ambientLight(50);
+  
+  ambientLight(128);
+  directionalLight(0, 255, 0, 0.25, 0.25, 0);
+  /*
   directionalLight(255, 0, 0, 0.25, 0.25, 0);
   pointLight(0, 0, 255, locX, locY, 250);
-
+*/
+/*
   push();
   translate(-width / 4, 0, 0);
   rotateZ(frameCount * 0.02);
@@ -21,8 +25,25 @@ function draw() {
   specularMaterial(250);
   box(100, 100, 100);
   pop();
+  */
+  push();
+  polygon(0, 0, 21, 6);
+  pop();
+  
+  
 
   translate(width / 4, 0, 0);
   ambientMaterial(250);
   sphere(120, 64);
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
