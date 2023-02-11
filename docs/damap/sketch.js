484ccbbc968af207cca077566f7c0c1226d4f417
martingo <110309286+martingo@users.noreply.github.com>
2023-02-11 12:03:19 -0300

    cosas y hexagon

--- docs/damap/sketch.js
+++ docs/damap/sketch.js
@@ -1,3 +1,9 @@
+let daTile;
+
+function preload() {
+
+  daTile = loadImage('images/hexagon.png');
+}
 function setup() {
   
   createCanvas(710, 400, WEBGL);
@@ -7,34 +13,39 @@ function draw() {
   
   background(0);
 
+/*
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
 
   
   ambientLight(128);
   directionalLight(0, 255, 0, 0.25, 0.25, 0);
-  /*
+  
   directionalLight(255, 0, 0, 0.25, 0.25, 0);
   pointLight(0, 0, 255, locX, locY, 250);
-*/
-/*
+
   push();
   translate(-width / 4, 0, 0);
   rotateZ(frameCount * 0.02);
   rotateX(frameCount * 0.02);
   specularMaterial(250);
   box(100, 100, 100);
-  pop();
-  */
-  push();
-  polygon(0, 0, 21, 6);
-  pop();
-  
-  
+  pop();  
 
   translate(width / 4, 0, 0);
   ambientMaterial(250);
   sphere(120, 64);
+  */
+  
+  push();
+  translate(-200, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  texture(daTile);
+  plane(100, 100);
+  pop();
+
 }
 
 function polygon(x, y, radius, npoints) {
